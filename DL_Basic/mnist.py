16fdc927f665b7a703cabdc20f9ec094eb03b8cd
DucTriCE <trild.ce@gmail.com>
2023-07-27 19:10:35 +0700

    Add updates

---
+++ DL_Basic/mnist.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+n_epochs = 3
